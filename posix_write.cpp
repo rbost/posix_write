ee70a8bf4c7816a6a844ed8a40ea9c074295aa05
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-04-10 21:17:15 +0200

    Test pwrite too.

--- posix_write.cpp
+++ posix_write.cpp
@@ -36,6 +36,36 @@ void open_and_write(const std::string &filename, const size_t block_size,
   free(block);
 }
 
+void open_and_pwrite(const std::string &filename, const size_t block_size,
+                     const char fill_byte) {
+  char *block = (char *)malloc(block_size);
+
+  std::fill(block, block + block_size, fill_byte);
+
+  int flags = (O_CREAT | O_TRUNC | O_RDWR);
+
+  int fd = open(filename.c_str(), flags, 0644);
+  if (fd != -1) {
+    ssize_t ret = -1;
+    ret = pwrite(fd, block, block_size, 10);
+
+    if (ret < 0) {
+      std::cerr << "Error when writing file (with pwrite) " << filename
+                << "; errno " << errno << "\n ";
+    }
+  } else {
+    std::cerr << "Error when opening file " << filename << "; errno " << errno
+              << "\n";
+  }
+  int err_close = close(fd);
+  if (err_close != 0) {
+    std::cerr << "Error when closing file " << filename << "; errno " << errno
+              << "\n";
+  }
+
+  free(block);
+}
+
 int main(int argc, char const *argv[]) {
   // std::cerr << "Small write test ...";
   // open_and_write("small_block.bin", 1024, 0x01);
@@ -51,6 +81,14 @@ int main(int argc, char const *argv[]) {
 
   std::cerr << "2^31 write test ...";
   open_and_write("2_31_block.bin", 1UL << 31, 0x03);
+  std::cerr << "done\n";
+
+  std::cerr << "2^31-1 pwrite test ...";
+  open_and_pwrite("2_31_1_block.bin", (1UL << 31) - 1, 0x02);
+  std::cerr << "done\n";
+
+  std::cerr << "2^31 pwrite test ...";
+  open_and_pwrite("2_31_block.bin", 1UL << 31, 0x03);
   std::cerr << "done\n";
 
   // std::cerr << "2^32 write test ...";
